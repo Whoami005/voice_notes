92748bb58b1a7537bb33b185597413a6c1dd80e4
Whoami05 <46159852+Whoami005@users.noreply.github.com>
2025-12-28 19:25:03 +0300

    Обновил main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:voice_notes/core/packages/app_router/app_router.dart';
 import 'package:voice_notes/core/theme/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  final appRouter = AppRouter.createRouter();
+
+  runApp(MyApp(router: appRouter));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GoRouter router;
+
+  const MyApp({required this.router, super.key});
 
   @override
   Widget build(BuildContext context) {
-    final appRouter = AppRouter.createRouter();
-
     return MaterialApp.router(
       title: 'Voice Notes',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.dark,
-      routerConfig: appRouter,
+      routerConfig: router,
     );
   }
 }
