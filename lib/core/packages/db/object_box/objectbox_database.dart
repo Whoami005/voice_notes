33c7f50375cfde66ad3f725a2f880f465284beef
Whoami05 <46159852+Whoami005@users.noreply.github.com>
2025-12-30 23:20:34 +0300

    Инициализировал бд

---
+++ lib/core/packages/db/object_box/objectbox_database.dart
@@ -0,0 +1,32 @@
+import 'package:path/path.dart' as p;
+import 'package:voice_notes/core/packages/db/object_box/objectbox.g.dart';
+import 'package:voice_notes/core/packages/path/app_path_provider.dart';
+
+/// Абстракция для тестирования и гибкости
+abstract interface class DatabaseClient {
+  Box<T> box<T>();
+
+  Future<void> close();
+}
+
+/// Реализация ObjectBox
+class ObjectBoxDatabase implements DatabaseClient {
+  late final Store _store;
+
+  ObjectBoxDatabase._(this._store);
+
+  static Future<ObjectBoxDatabase> create() async {
+    final doc = await AppPathProvider.getApplicationDocumentsDirectory();
+    final dbDirectory = p.join(doc.path, 'voice-notes-box_db');
+
+    final store = await openStore(directory: dbDirectory);
+
+    return ObjectBoxDatabase._(store);
+  }
+
+  @override
+  Box<T> box<T>() => _store.box<T>();
+
+  @override
+  Future<void> close() async => _store.close();
+}
